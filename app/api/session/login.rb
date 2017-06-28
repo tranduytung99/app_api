249fed485d9e2469a5313fc9e04f78e08d9ff242
Duy Tung <zduytungzzz@gmail.com>
2017-06-28 08:00:05 +0700

    demo api

--- app/api/session/login.rb
+++ app/api/session/login.rb
@@ -1,62 +1,57 @@
-module Session
-  class Login < Grape::API
-    version 'v1', using: :path
-    format :json
+# module Session
+#   class Login < Grape::API
+#     version 'v1', using: :path
+#     format :json
 
-    helpers do
+#     helpers do
+#       def user_params
+#         params = ActionController::Parameters.new({
+#          user: {
+#           email: params[:email],
+#           password: params[:password]
+#          }
+#         })
+#         params.require(:user).permit(:email, :password)
+#       end
 
-      def user_params
-        params = ActionController::Parameters.new({
-         user: {
-          email: params[:email],
-          password: params[:password]
-         }
-        })
-        params.require(:user).permit(:email, :password)
-      end
+#       def create
+#         @user = User.new
+#         if @user.save
+#         else
+#         end
+#       end
 
-      def create
+#       def current_user
+#         @user = User.find_by_email params[:email]
+#         if @user.present? && @user.valid_password?(params[:password])
+#         else
+#           error_msg = 'Bad Authentication Parameters'
+#           error!({ 'error_msg' => error_msg }, 401)
+#         end
+#       end
+#     end
 
-        @user = User.new
+#     resource :sign_up do
+#       post :user_sign_up do
+#         create
+#         {
+#           user: @user
+#         }
+#       end
+#     end
 
-        if @user.save
-        else
-
-        end
-      end
-
-      def current_user
-        @user = User.find_by_email params[:email]
-        if @user.present? && @user.valid_password?(params[:password])
-        else
-          error_msg = 'Bad Authentication Parameters'
-          error!({ 'error_msg' => error_msg }, 401)
-        end
-      end
-    end
-
-    resource :sign_up do
-
-      post :user_sign_up do
-        create
-        {
-          user: @user
-        }
-      end
-    end
-
-    resource :login do
-      params do
-        requires :email, type: String
-        requires :password, type: String
-      end
-      get :session do
-        current_user
-        {
-          current: @user,
-          email: params[:email]
-        }
-      end
-    end
-  end
-end
+#     resource :login do
+#       params do
+#         requires :email, type: String
+#         requires :password, type: String
+#       end
+#       get :session do
+#         current_user
+#         {
+#           current: @user,
+#           email: params[:email]
+#         }
+#       end
+#     end
+#   end
+# end
